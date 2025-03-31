3a8a4afde5469cbc035651dfa81485c1e6ad8883
Eric <efalchier@industrydive.com>
2025-03-31 10:05:48 -0400

    update url to re_path

--- locking/urls.py
+++ locking/urls.py
@@ -1,15 +1,15 @@
 from __future__ import absolute_import, unicode_literals, division
 
-from django.conf.urls import url
+from django.urls import re_path
 
 from .api import LockAPIView
 
 __all__ = ('urlpatterns', )
 
 urlpatterns = [
-    url(r'api/lock/(?P<app>[\w-]+)/(?P<model>[\w-]+)/$',
+    re_path(r'api/lock/(?P<app>[\w-]+)/(?P<model>[\w-]+)/$',
         LockAPIView.as_view(), name='locking-api'),
 
-    url(r'api/lock/(?P<app>[\w-]+)/(?P<model>[\w-]+)/(?P<object_id>\d+)/$',
+    re_path(r'api/lock/(?P<app>[\w-]+)/(?P<model>[\w-]+)/(?P<object_id>\d+)/$',
         LockAPIView.as_view(), name='locking-api'),
 ]
